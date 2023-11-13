b03d558f8e991b9234542863f1cdda4289a5a093
PaulaPeraltaA <paupagui@espol.edu.ec>
2023-11-13 18:51:25 -0500

    p_sentencia

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -5,9 +5,27 @@ def p_print(p):
   '''print : FMT_LIBRARY DOT PRINTLN LPAREN value RPAREN
           | FMT_LIBRARY DOT PRINTF LPAREN value RPAREN'''
 
+def p_sentencia(p):
+  '''sentencia : impresion
+              | asignacion
+              | mientras'''
+  
 def p_asignacion(p):
   '''asignacion : VAR IDENTIFIER data_type EQUAL value
                 | CONST IDENTIFIER data_type EQUAL value'''
+  
+def p_impresion(p):
+  "impresion : IMPRESION P_IZQ  valor P_DER"
+
+def p_impresion_sinvalor(p):
+  "impresion : IMPRESION P_IZQ P_DER"
+
+def p_mientras(p):
+  "mientras : MIENTRAS P_IZQ VERDADERO P_DER DOSP sentencia"
+
+def p_valores(p):
+  '''valores : valor
+            | valores COMA valor'''
 
 def p_data_type(p):
   '''data_type : STRING
