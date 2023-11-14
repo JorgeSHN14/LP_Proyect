fb132736063ebaf2483482abd2d1906efc8dfa9c
JorgeSHN14 <joheniet@espol.edu.ec>
2023-11-13 19:29:25 -0500

    a

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -3,7 +3,12 @@ from lex_analyzer import tokens
 
 def p_print(p):
   '''print : FMT_LIBRARY DOT PRINTLN LPAREN value RPAREN
-          | FMT_LIBRARY DOT PRINTF LPAREN value RPAREN'''
+           | FMT_LIBRARY DOT PRINTF LPAREN value RPAREN'''
+
+
+def p_identifiers(p):
+  '''identifiers
+  '''
 
 def p_asignacion(p):
   '''asignacion : VAR IDENTIFIER data_type EQUAL value
@@ -11,11 +16,11 @@ def p_asignacion(p):
 
 def p_data_type(p):
   '''data_type : STRING
-   | INTEGER_DATA_TYPE
-   | FLOAT32_DATA_TYPE
-   | FLOAT64_DATA_TYPE
-   | BOOLEAN_DATA_TYPE
-   | STRING_DATA_TYPE'''
+               | INTEGER_DATA_TYPE
+               | FLOAT32_DATA_TYPE
+               | FLOAT64_DATA_TYPE
+               | BOOLEAN_DATA_TYPE
+               | STRING_DATA_TYPE'''
 
 def p_value(p):
   '''value : STRING
