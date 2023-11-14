1dd1942bf8cfe65bf4fbc26a03cb3436afd5c67f
JorgeSHN14 <joheniet@espol.edu.ec>
2023-11-13 20:56:57 -0500

    xd

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,31 +1,30 @@
 import ply.yacc as sint
 from lex_analyzer import tokens
 
-def p_print(p):
-  '''print : FMT_LIBRARY DOT PRINTLN LPAREN value RPAREN
-           | FMT_LIBRARY DOT PRINTF LPAREN value RPAREN'''
-
-
-def p_identifiers(p):
-  '''identifiers
-  '''
-
 def p_sentencia(p):
   '''sentencia : impresion
               | asignacion
               | mientras
               | definicion_funcion
               | llamada_funcion'''
+
+def p_print(p):
+  '''print : FMT_LIBRARY DOT PRINTLN LPAREN value RPAREN
+           | FMT_LIBRARY DOT PRINTF LPAREN value COMMA RPAREN
+           | FMT_LIBRARY DOT PRINTF LPAREN value COMMA identifiers RPAREN'''
+  
+def p_print_whitoutvalue(p):
+  '''print : FMT_LIBRARY DOT PRINTLN LPAREN RPAREN
+            | FMT_LIBRARY DOT PRINTF LPAREN RPAREN'''
+
+
+def p_identifiers(p):
+  '''identifiers : IDENTIFIER
+                 | identifiers COMMA identifier'''
   
 def p_asignacion(p):
   '''asignacion : VAR IDENTIFIER data_type EQUAL value
                 | CONST IDENTIFIER data_type EQUAL value'''
-  
-def p_impresion(p):
-  "impresion : IMPRESION P_IZQ  valor P_DER"
-
-def p_impresion_sinvalor(p):
-  "impresion : IMPRESION P_IZQ P_DER"
 
 def p_mientras(p):
   "mientras : MIENTRAS P_IZQ VERDADERO P_DER DOSP sentencia"
